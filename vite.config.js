972dcc1c35048a87da2472085fe8276e1484e96e
Umar8507 <umarasghar518@gmail.com>
2026-03-06 23:41:56 +0500

    Upload all project files

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This 'base' ensures that all your CSS, JS, and images 
-  // load correctly from the subfolder on GitHub Pages.
   base: "/truckingsite-preview/", 
 })
